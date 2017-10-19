627d75511d4a4261c6bfaa4dc83775659906e281
sir3n-sn <sir3n.sn@gmail.com>
2017-10-19 17:03:14 +0300

    basicTest

---
+++ app/tests/test_flask_app.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+
+import app
+
+class BasicTest(unittest.TestCase):
+    '''Test if flask app returns correct results'''
+
+
+    def SetUp(self):
+        self.app = app.test_client()
+
+    def  test_main_page(self):
+        '''Tests flask redirects with no errors'''
+        response = self.app.get('/', follow_redirects=True, content_type='html/text')
+        self.assertEqual(response.status_code, 200)
