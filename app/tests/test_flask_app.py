c375348aff5ea33656214ed78bbd948d45788ee5
sir3n-sn <sir3n.sn@gmail.com>
2017-10-19 17:35:11 +0300

    FixedPytestIssue

--- app/tests/test_flask_app.py
+++ app/tests/test_flask_app.py
@@ -7,10 +7,10 @@ class BasicTest(unittest.TestCase):
     '''Test if flask app returns correct results'''
 
 
-    def SetUp(self):
+    def setUp(self):
         self.app = app.test_client()
 
-    def  test_main_page(self):
+    def  test_status(self):
         '''Tests flask redirects with no errors'''
         response = self.app.get('/', follow_redirects=True, content_type='html/text')
         self.assertEqual(response.status_code, 200)
